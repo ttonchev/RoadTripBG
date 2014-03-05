b340d3a79114da07bd3da73a8e3a14f0c8caee75
Boris Minev <boriss.minev@gmail.com>
2014-03-05 23:25:19 +0200

    initial commit

---
+++ RoadTripBG/test/com/roadtripbg/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.roadtripbg;
+
+import com.roadtripbg.MainActivity;
+import com.roadtripbg.R;
+import org.robolectric.RobolectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void shouldHaveHappySmiles() throws Exception {
+        String hello = new MainActivity().getResources().getString(R.string.app_name);
+        assertThat(hello, equalTo("RoadTripBG"));
+    }
+}
